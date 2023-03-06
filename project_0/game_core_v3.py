771e8d9c23f00f2be083c20f6ada7bfd1778543e
MachineMindset <python.codemaster01@gmail.com>
2023-03-06 15:40:04 -0600

    game_core_v3 prediction checker

--- project_0/game_core_v3.py
+++ project_0/game_core_v3.py
@@ -28,3 +28,28 @@ def game_core_v3(number: int = 1) -> int:
         
     return count 
 
+def score_game(game_core_v3) -> int:
+    """За какое количество попыток в среднем из 1000 подходов угадывает наш алгоритм
+
+    Args:
+        game_core_v3 ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+
+    count_ls = [] # список для сохранения количества попыток
+    np.random.seed(1) # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000)) # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(game_core_v3(number))
+
+    score = int(np.mean(count_ls)) # находим среднее количество попыток
+
+    print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
+    return(score)
+
+# RUN
+if __name__ == '__main__':
+    score_game(game_core_v3)
