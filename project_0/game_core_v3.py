4dca2c1c77d6317c6f6196eddf77acd67c4117f0
MachineMindset <python.codemaster01@gmail.com>
2023-03-06 15:34:57 -0600

    new file game_core_v3 + requirement update

--- project_0/game_core_v3.py
+++ project_0/game_core_v3.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 def game_core_v3(number: int = 1) -> int:
-    """
+    """Угадывает число с помощью двоичного поиска
+    
     Args:
         number (int, optional): Загаданное число. Defaults to 1.
 
@@ -9,16 +10,21 @@ def game_core_v3(number: int = 1) -> int:
         int: Число попыток
     """
     count = 0
-    low = 1
-    high = 100
+    low = 1       # Нижний порог
+    high = 100    # верхний порог
     
     while True:
         predict = (low + high) // 2
         count += 1
         
-        if predict == number:
+        if predict == number: 
             break
         
         elif predict > number:
-            
-               
+            high = predict - 1 
+        
+        else:
+            low = predict + 1
+        
+    return count 
+
